fa6fbbbffbfa44bac239d96ee59b3d3daa143081
Lucas Pruvost <intoxx@protonmail.com>
2020-11-05 16:50:20 +0100

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import GlobalStyle from '../components/GlobalStyle.svelte';
+	import Nav from '../components/Nav.svelte';
+
+	export let segment: string;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<GlobalStyle />
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
